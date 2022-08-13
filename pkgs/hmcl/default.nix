76eea0cda4c283d0cba9854ad103ac4152fd2709
zrah <zrah@protonmail.com>
2022-08-13 23:09:56 +0800

    pkg: add hmcl icon

--- pkgs/hmcl/default.nix
+++ pkgs/hmcl/default.nix
@@ -1,5 +1,5 @@
 { lib, stdenvNoCC, fetchurl, jre, libpulseaudio, alsa-lib, flite, makeWrapper
-, ... }:
+, makeDesktopItem, ... }:
 let version = "3.5.3.221";
 in stdenvNoCC.mkDerivation rec {
   name = "hmcl";
@@ -10,6 +10,12 @@ in stdenvNoCC.mkDerivation rec {
     sha256 = "sha256-siGq4bdMZQAB7xF3gpI3Qir0orjVWACmUU7XpattFzk=";
   };
 
+  icon = fetchurl {
+    url =
+      "https://raw.githubusercontent.com/huanghongxun/HMCL/da66102bc02d4e3c1adf2e10736950326218c1e2/HMCL/src/main/resources/assets/img/icon.png";
+    sha256 = "sha256-JJatyGMbs6tC6lpzfis5JC5qHKhqkIVffSBKCG3jX+k=";
+  };
+
   dontUnpack = true;
   runtimeLibraryPath = lib.makeLibraryPath [ libpulseaudio alsa-lib flite ];
 
@@ -18,11 +24,21 @@ in stdenvNoCC.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/{bin,share/HMCL}
     cp $src $out/share/HMCL/HMCL.jar
+    install -Dm644 $icon $out/share/icons/hicolor/32x32/apps/hmcl.png
     makeWrapper ${jre}/bin/java $out/bin/hmcl \
     --add-flags "-jar $out/share/HMCL/HMCL.jar" \
     --run "mkdir -p ~/.config/hmcl  && cd ~/.config/hmcl" \
     --prefix LD_LIBRARY_PATH : "${runtimeLibraryPath}"
   '';
+
+  desktopItem = makeDesktopItem {
+    type = "Application";
+    name = "hmcl";
+    desktopName = "HMCL";
+    exec = "hmcl";
+    icon = "hmcl";
+  };
+  desktopItems = [ desktopItem ];
 
   meta = with lib; {
     description = "A Minecraft Launcher which is multi-functional";
